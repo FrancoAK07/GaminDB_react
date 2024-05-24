b747477aa612c300e2728e4686f2a8add806fbaa
Franco <franco_rivo7@hotmail.com>
2024-05-24 14:29:31 -0400

    updated home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -15,27 +15,31 @@ function Home() {
 	return (
 		<div className="home h-100">
 			<div className="home-background position-relative w-100">
-				<div className="home-gradient w-100 position-absolute"></div>
+				<div className="home-gradient h-100 w-100 position-absolute"></div>
 				<img className="h-100 w-100" src={require("../../assets/images/sc5r7v.png")} alt="" />
 				<div className="position-absolute top-0 w-100">
 					<div className="container mb-5">
-						<div className="row text-center text-white mb-4 pt-4 ">
+						<div className="row w-100 m-auto text-center text-white my-4 ">
 							<h1>GamingDB</h1>
 						</div>
-						<div className="row text-start headlines rounded-3 p-3">
-							<h2 className="text-white">
-								<img src={list} alt="list" /> Create your list of games
-							</h2>
-							<h2 className="text-white">
-								<img src={list} alt="list" /> Review games and read reviews from other players
-							</h2>
+						<div className="row w-100 m-auto text-start ">
+							<div className="col-12 p-0">
+								<h2 className="text-white headlines rounded-3 p-3 w-auto">
+									<img src={list} alt="list" /> Create your list of games
+								</h2>
+							</div>
+							<div className="col-12 p-0">
+								<h2 className="text-white headlines rounded-3 p-3">
+									<img src={list} alt="list" /> Review games and read reviews from other players
+								</h2>
+							</div>
 						</div>
 						<div className="row text-center w-50 ms-auto me-auto mt-3">
-							<div className="col">
-								<div className="row">
-									<h1 className="text-light">New Games</h1>
+							<div className="col p-0 w-100">
+								<div className="row m-auto">
+									<h1 className="text-light p-0">New Games</h1>
 								</div>
-								<div className="row row-cols-2 row-cols-md-4">
+								<div className="row row-cols-2 row-cols-md-4 w-100 m-auto">
 									{recentGames.length
 										? recentGames.map((game, i) => {
 												return (
